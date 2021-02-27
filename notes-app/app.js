74b234d4fbea4683c1c9b8d4d3048dd8ad2d6073
Utkarsh Sharma <rickysharma1111@gmail.com>
2021-02-27 14:08:00 +0530

    initial

---
+++ notes-app/app.js
@@ -0,0 +1,10 @@
+// const chalk = require('chalk')
+const notes = require("./notes.js")
+// const validator =  require("validator")
+
+const message =  notes;
+
+
+// console.log(chalk.blue(message()))
+console.log(validator.isEmail("rickysharma1111@gmail.com"))
+// console.log(validator)
